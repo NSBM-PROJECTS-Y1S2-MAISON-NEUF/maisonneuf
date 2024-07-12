004bcd3be0619b2983c809bc0886f8520179ec56
tharundi <pawanitharuni24@gmail.com>
2024-07-12 16:40:39 +0530

    Added Login Functionalities

--- php/login.php
+++ php/login.php
@@ -6,21 +6,22 @@ include 'db.php';
 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST["username"];
-    $password = $_POST["password"];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-    // $_SESSION['username'] = $username;
+    $_SESSION['username'] = $username;
 
 
-    $sql = "SELECT * FROM Users WHERE Username='$username' AND Password='$password'";
+    $sql = "SELECT * FROM users WHERE Email='$username' AND Password='$password'";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
-        echo "Login successful!";
+        echo "success";
     } else {
         echo "Invalid username or password.";
     }
 
     $conn->close();
 }
+
 ?>
